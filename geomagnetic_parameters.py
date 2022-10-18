8552292d96cd1ee66d7aeb263dbadc9b0bd43a92
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-10-18 11:29:41 -0300

    Removing string manipulation and adding an new convertion

--- geomagnetic_parameters.py
+++ geomagnetic_parameters.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.constants as const
 import datetime
 import time
+import spacepy.coordinates as coord
+from spacepy.time import Ticktock
+import pandas as pd
 
 def toYearFraction(date):
     
@@ -35,70 +38,73 @@ def dipole_magnetic_field(colatitude, h = 1):
 def colatitude(latitude):
     return (np.pi / 2) - latitude
 
-date = datetime.datetime(2019, 1, 1)
 
-def dip(latitude, longitude, date, altitude = 1):
 
-    date_fraction = toYearFraction(date)
+def dip(inclination):
+   """Latitude inlicação magnética (dip) """
+   return np.degrees(np.arctan(0.5*np.tan(np.radians(inclination)) / 2))
+
+def geo_to_mag(geo_lat, geo_lon, date):
+    """
+    return the magnetic coords in the 
+    same units as the geographic"""
     
-    D, I, H, X, Y, Z, F = pyIGRF.igrf_value(latitude, 
-                                longitude, 
-                                altitude, 
-                                date_fraction)
+    Re = 6378.0 #mean Earth radius in kilometers
+     
+    cvals = coord.Coords([float(Re), 
+                          float(geo_lat), 
+                          float(geo_lon)], 
+                         'GEO', 'sph', 
+                         ['Re','deg','deg'])
     
-    # Latitude inlicação magnética (dip)
-    return np.degrees(np.arctan(np.tan(np.radians(I)) / 2))
-
-
-
-
+    date = date.strftime("%Y-%m-%dT%H:%M")
+    cvals.ticks = Ticktock([date], 'UTC')
     
-import spacepy.coordinates as coord
-from spacepy.time import Ticktock
+    dat_coords =  cvals.convert('MAG','sph').data[0]
+    
+    mag_lat = dat_coords[1]
+    mag_lon = dat_coords[2]
+    
+    return (mag_lat, mag_lon)
 
-def string_to_list(alt, lat, lon, date):
+
+
+
+
+def mag2geo(mag_lat, mag_lon, date):
     
     """
     Convert the results from 'geo_to_mag' functions
     from numeric tuple
     
     """
-
-    first = str(geo_to_mag(alt, lat, lon, date))
     
-    start = first.find('[[')
-    end = first.find(']]')
-    last = first[start + 2:end].split(', ')
+    r = 6378.0
     
-    list_as_numeric = [float(num) for num in last]
+    date = date.strftime("%Y-%m-%dT%H:%M")
     
-    lat_mag = list_as_numeric[1]
-    lon_mag = list_as_numeric[2]
-    return round(lat_mag, 2), round(lon_mag, 2)
-
-
-def geo_to_mag(alt, lat, lon, date):
-    #call with altitude in kilometers and lat/lon in degrees 
+    cvals = coord.Coords([float(r), 
+                          float(mag_lat), 
+                          float(mag_lon)], 
+                         'MAG', 'sph', 
+                         ['Re','deg','deg'])
     
-    Re = 6378.0 #mean Earth radius in kilometers
     
-    #setup the geographic coordinate object with altitude in earth radii 
-    cvals = coord.Coords([float(alt + Re), 
-                          float(lat), float(lon)], 
-                         'GEO', 'sph', ['Re','deg','deg'])
-    
-    date = str(date).replace(' ', 'T')
-    #set time epoch for coordinates:
     cvals.ticks = Ticktock([date], 'UTC')
     
-    #return the magnetic coords in the same units as the geographic:
-    return cvals.convert('MAG','sph')
+    dat_coords = cvals.convert('GEO','sph').data[0]
+    
+    geo_lat = dat_coords[1]
+    geo_lon = dat_coords[2]
+    
+    
+    return (geo_lat, geo_lon)
 
-latitude = np.arange(-30, 30, 1)
+mag_lat = 0
+mag_lon = -40
 
-#:
-mag_lats = [string_to_list(700, lat, 0, date)[0] 
-            for lat in latitude]
+date = datetime.datetime(2014, 1, 1)
+lat, lon = mag2geo(mag_lat, mag_lon, date)
 
-for alt in range(0, 1000, 100):
-    plt.plot(mag_lats, dipole_magnetic_field(mag_lats, h = alt))
+
+print(lat, lon)
