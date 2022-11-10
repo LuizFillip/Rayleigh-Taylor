8bb5bf6684e6c72dabaf1d2779eb0ddcd3ef2d11
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-10 11:21:31 -0300

    Labeling updates

--- plot/plotAnnualVariations.py
+++ plot/plotAnnualVariations.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+import matplotlib.ticker as ticker
+
 from plotConfig import *
 
 df = pd.read_csv("database/growthRates/Gammas.txt", 
@@ -9,15 +12,27 @@ df.index = pd.to_datetime(df.index)
 
 df = df.loc[df.index.year == 2014]
 
-fig, ax = plt.subplots(figsize = (14, 6))
+fig, ax = plt.subplots(figsize = (20, 10))
 
-df["nowind"].plot()
-df["noreco"].plot()
-df["nowindReco"].plot()
-df["local"].plot()
-df["all"].plot()
+args = dict(lw = 2)
+
+ax.plot(df["nowind"], label = "$U = 0$", **args)
+ax.plot(df["noreco"], label = "$R = 0$", **args)
+ax.plot(df["nowindReco"], label =  "$U = R = 0$", **args)
+ax.plot(df["local"], label = "$U = V_z = R = 0$", **args)
+ax.plot(df["all"],  label = "Todos parâmetros", **args)
 
 
-ax.set(ylim = [-0.5e-3, 3e-3])
+ax.set(ylim = [-0.5e-3, 3e-3], 
+       xlabel = "Meses", 
+       ylabel = "$\gamma_{RT}~ (\\times 10^{-3} s^{-1})$")
 
 
+ax.legend(ncol = 3, fontsize = 30)
+
+ax.xaxis.set_major_formatter(dates.DateFormatter('%b'))
+ax.xaxis.set_major_locator(dates.MonthLocator(interval = 1))
+
+
+ax.yaxis.set_major_formatter(
+    ticker.FuncFormatter(lambda y, _: '{:g}'.format(y/10e-3)))
