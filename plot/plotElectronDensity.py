0ad2398e25f2c38a3c4c94d13ae5bf2cb90a3e19
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-11 14:52:08 -0300

    Adding letters infos

--- plot/plotElectronDensity.py
+++ plot/plotElectronDensity.py
@@ -3,7 +3,7 @@ from generalized_growth_rate import getPyglow, length_scale_gradient
 from datetime import datetime
 import matplotlib.pyplot as plt
 from plotConfig import *
-
+import matplotlib.ticker as ticker
 
 date = datetime(2014, 1, 1, 21, 10)
 
@@ -37,3 +37,8 @@ ax[1].xaxis.set_major_formatter(
     ticker.FuncFormatter(lambda y, _: '{:g}'.format(y/10e-3)))
 ax[1].set(xlabel = ("Gradiente de escala (L) \n" + 
        r"$\frac{1}{n_0} \frac{\partial n_0}{\partial z} (10^{-3} m^{-1})$"))
+
+text_painels(ax, x = 0.05, y = 0.95, 
+                 fontsize = 45)
+fig.savefig(path_tex["latex"] + "electron_density.png", 
+            dpi = 500)
