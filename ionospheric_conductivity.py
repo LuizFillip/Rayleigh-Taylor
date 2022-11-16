2f6a3711d836a3f4a72144692e1ea0905592f402
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-15 22:32:19 -0300

    Removing functions

--- ionospheric_conductivity.py
+++ ionospheric_conductivity.py
@@ -1,44 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.constants as const
-from datetime import datetime
-import pandas as pd
-from nrlmsise00 import msise_flat
-from iri2016 import IRI
-
-from sys import exit
-
-def get_concentrations_profiles(TN, 
-                                o_point, 
-                                o2_point, 
-                                n2_point, 
-                                DZ):
-    
-    JMAX = len(TN)
-    
-    CO = np.zeros(JMAX)                 # creating 1d array
-    CO2 = np.zeros(JMAX)                # creating 1d array
-    CN2 = np.zeros(JMAX)                # creating 1d array
-
-    
-    HB = 200.0                          # F region base  height [km]
-
-    
-    for J in range(0, JMAX):
-        Z1 = HB + DZ * J
-        GR = 1.0 / (1.0 + Z1 / 6370.0) ** 2.0
-        HO = 0.0528 * TN[J] / GR                               # scale height of O [km]
-        HO2 = 0.0264 * TN[J] / GR                              # scale height of O2 [km]
-        HN2 = 0.0302 * TN[J] / GR                              # scale height of N2 [km]
-
-        p_co = np.float64(o_point / 5.33 * 8.55)
-        p_co2 = np.float64(o2_point / 1.67 * 4.44)
-        p_cn2 = np.float64(n2_point / 9.67 * 2.26)
-
-        CO[J] = p_co * np.exp(-(Z1 - 335.0) / HO)           # atomic oxygen [cm-3]
-        CO2[J] = p_co2 * np.exp(-(Z1 - 335.0) / HO2)        # molecular oxygen [cm-3]
-        CN2[J] = p_cn2 * np.exp(-(Z1 - 335.0) / HN2) 
-    return CO, CO2, CN2
 
 
 def ion_neutral_collision(Tn, O, O2, N2):
@@ -62,14 +23,6 @@ def ion_cyclotron(B = 0.285e-04):
     """Ion gyro frequency"""
     return (const.elementary_charge * B / const.proton_mass)
 
-def electron_ratio(nu_e, B = 0.285e-04):
-    """Electron ratio cyclotron frequency and collision"""
-    return (-const.elementary_charge * B) / (const.electron_mass * nu_e)
-
-def ion_ratio(nu_i, B = 0.285e-04):
-    """Ion ratio cyclotron frequency and collision"""
-    return (const.elementary_charge * B) / (const.proton_mass * nu_i)
-
 def electron_mobility(nu_e):
     """Electric mobility for electrons (mass transport)"""
     return (-const.elementary_charge) / (const.electron_mass * nu_e)
@@ -77,6 +30,16 @@ def electron_mobility(nu_e):
 def ion_mobility(nu_i):
     """Electric mobility for ions (mass transport)"""
     return (const.elementary_charge) / (const.proton_mass * nu_i)
+
+
+def electron_ratio(nu_e, B = 0.285e-04):
+    """Electron ratio cyclotron frequency and collision"""
+    return electron_cyclotron(B) / nu_e
+
+def ion_ratio(nu_i, B = 0.285e-04):
+    """Ion ratio cyclotron frequency and collision"""
+    return ion_cyclotron(B) / nu_i
+
     
 def parallel_conductivity(Ne, nu_e, nu_i):
     """conductivity along magnetic field"""
@@ -93,8 +56,14 @@ def Pedersen_conductivity(Ne, nu_e, nu_i, B = 0.285e-04):
     
     return Ne * const.elementary_charge * (ion_term - electron_term)
 
+def dl(lat, dzeta, Re = 6.371009e6, L = 1):
+    zeta = np.sin(np.radians(lat))
+    return pow((1 + 3 * zeta**2), 0.5) * Re * L * dzeta
+    
+def pedersen(Ne, nui, B):
+    return Ne * const.proton_mass * nui / B**2
 
-
+   
 def Hall_conductivity(Ne, nu_e, nu_i, B = 0.285e-04):
     """Conductivity perpendicular to both electric and magnetic field"""
     electron_term = electron_ratio(nu_e, B = B)**2 / (1 + electron_ratio(nu_e, B = B)**2)
@@ -104,82 +73,5 @@ def Hall_conductivity(Ne, nu_e, nu_i, B = 0.285e-04):
     return (Ne * const.elementary_charge / B) * (electron_term - ion_term)
 
 
-def main():
-    
-    
-    
-    
-    def get_profiles(glat, glon, date, alts):
-        
-        res = msise_flat(date, alts[None, :], glat, glon, 100, 150, 4)
-        
-        
-        columns= ["He", "O", "N2", "O2", "Ar", "mass", 
-                  "H", "N", "AnO", "Tex", "Tn"]
-        
-        df = pd.DataFrame(res[0], 
-                          index = alts, 
-                          columns = columns)
-        
-        iri = IRI(date, (hmin, hmax - step, step), glat, glon)
-        
-        Ne = iri.ne.values
-        Te = iri.Te.values
 
-        df["nui"] = ion_neutral_collision(df.Tn, df.O, 
-                                          df.O2, df.N2)
-        
-        df["Nn"] = df[["He", "O", "N2", 
-                       "O2", "Ar", "H", "N"]].sum(axis = 1)
-        
-        df["nue"] = electron_neutral_collision(Te, df.Nn)
-        
-        return Pedersen_conductivity(Ne, df.nue, df.nui)
-    
 
-    
-    
-    
-    step = 5
-    hmax = 600
-    hmin = 100
-    alts = np.arange(hmin, hmax, step)
-    date = datetime(2014, 1, 1, 23, 0, 0)
-    
-    glon = -40
-    
-    from geomagnetic_parameters import string_to_list
-    
-    
-    
-    def integrated_values(alts, date, glon = 0, lat_max = 30, delta = 5):
-        
-        
-        
-        out = []
-        for glat in np.arange(-2, lat_max + delta, delta):
-            mlat = string_to_list(0, glat, glon, date)[0]
-        
-            sigma_p = get_profiles(float(glat), glon, date, alts)
-            
-            sigma_1 = sigma_p.values * (1 + 3 * np.sin(np.radians(mlat))**2)
-            
-            out.append(sigma_1)
-        
-        Re = 6.371009e6
-        L = 1
-        
-        integrated = 2*Re*L*(np.sum(np.vstack(out).T, axis = 1))*delta
-       
-        return sigma_p.index, integrated
-    
-    altitude, sigma = integrated_values(alts, date, glon = 0)
-    
-    #(sigma)
-        
-    plt.plot(sigma, altitude)
-    
-    plt.xscale("log")
-    
-    #plt.yscale("log")
-main()
