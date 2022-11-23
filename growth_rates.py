9ba7ad786f5cb32130120dc8a28b279ee4453917
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-23 17:44:22 -0300

    Updating run_for_all_times function

--- growth_rates.py
+++ growth_rates.py
@@ -27,17 +27,18 @@ def growth_rate_RT(nu, L, R, Vp, U):
 
 def run_for_all_times(vz, 
                       date, 
-                      neInfile = "20140101.txt"):
+                      neInfile):
     """Running for all electron density (range 200 to 500 km)
     in whole day for each 10 minutes"""
 
     end =  date + datetime.timedelta(hours = 23, minutes = 50)
     
-    times = pd.date_range(date, 
-                          end, 
-                          freq = "10min")
+    times = pd.date_range(date,  end, freq = "10min")
     
-    out = []
+    out = {"g300_reco": [], 
+           "g300_Vp": [], 
+           "gVp_max": [], 
+           "gR_max": []}
     
     for date in times:
         
@@ -61,116 +62,56 @@ def run_for_all_times(vz,
         
         alts = dat.index.values
         
-        gamma = growth_rate_RT(nu, l, r, vz, 0)
-    
-        max_gamma = gamma[alts == 300 ]
-        out.append(np.nanmax(max_gamma))
+        # Without winds effects 
+        gammaR = growth_rate_RT(nu, l, r, vz, 0)
+        # Without Recombination and winds effects
+        gammaVp = growth_rate_RT(nu, l, 0, vz, 0)
         
-    return out
+        # Get from range altitude
+        gVp_max = np.nanmax(gammaVp[(alts > 250) & (alts < 350)])
+        gR_max = np.nanmax(gammaR[(alts > 250) & (alts < 350)])
+        
+        # Get from fixed altitude
+        gamma300R = gammaR[alts == 300]
+        gamma300Vp = gammaVp[alts == 300]
+        
+        out["g300_reco"].append(gamma300R[0])
+        out["g300_Vp"].append(gamma300Vp[0])
+        out["gVp_max"].append(gVp_max)
+        out["gR_max"].append(gR_max)
+        
+    return pd.DataFrame(out, index = times)
 
-def run_for_all_days2():
+
+preInfile = "database/PRE/FZ_PRE_2014_2015.txt"
+densityInfile = "D:\\ne\\"
+
+
+def run_for_all_days2(preInfile, densityInfile):
     gammas = []
-    dates = []
     
-    pre = PRE("database/PRE/FZ_PRE_2014_2015.txt").df
-    
+    pre = PRE(preInfile).df
     
     for num in tqdm(pre.index):
         
         try:
+            
             filename = num.strftime("%Y%m%d") + ".txt"
+            
             vz = pre.loc[num, "vz"]
             
             date = pd.to_datetime(num.date())
-            
             gammas.append(run_for_all_times(vz, date, 
-                          neInfile = "database/density/" + filename))
-        
-            dates.append(date)
+                                            densityInfile + filename))
+    
         except:
             continue
         
-    return gammas, dates
+    return pd.concat(gammas)
 
 
-gammas, dates = run_for_all_days2()
-#%%
-import plotConfig
+df = run_for_all_days2(preInfile, densityInfile)
 
-x = np.linspace(0, 24, 144)
-z = np.array(gammas).T
-y = dates
 
-fig, ax = plt.subplots(figsize = (30, 15))
-cs = ax.contourf(y, x, z, 30)
-
-import matplotlib.dates as md
-ax.xaxis.set_major_formatter(md.DateFormatter('%d-%m'))
-ax.xaxis.set_major_locator(md.DayLocator(interval = 10))
-
-cb = plt.colorbar(cs)
-
-cb.set_label(r'$\gamma_{RT}~(s^{-1})$')
-ax.set(yticks = np.arange(0, 25, 2),
-       ylabel = "Tempo (UT)", 
-        xlabel = "Meses")
-#%%
-
-def run_for_all_days(pre):
-    out_res = []
-    for i in range(len(pre.times)):
-       
-        date = pre.times[i]
-        
-        vz = pre.pre[i]
-        
-        pyglow = getPyglow(date)
-        
-        
-        ne = pyglow.density()
-        u = pyglow.winds()
-        dat = runMSISE(date)
-        
-        
-        neutral = neutrals(dat.Tn.values, 
-                           dat.O.values, 
-                            dat.O2.values, 
-                             dat.N2.values)
-        
-        nu = neutral.collision
-        r = neutral.recombination
-        
-        l = scale_gradient(ne*1e6)
-        
-        alts = dat.index.values
-        
-        GRT = growth_rate_RT(nu, l, r, vz, u)
-        wind_zero = growth_rate_RT(nu, l, r, vz, 0)
-        reco_zero = growth_rate_RT(nu, l, 0, vz, u)
-        reco_wind_zero = growth_rate_RT(nu, l, 0, vz, 0)
-        LRT = growth_rate_RT(nu, l, 0, 0, 0)
-        out_gammas = []
-        out_res.append(out_gammas)
-        
-        for elem in [GRT, wind_zero, reco_zero, 
-                     reco_wind_zero, LRT]:
-            
-        
-            max_gamma = elem[(alts > 250) & (alts < 350)]
-            #max_gamma = elem[alts == 300 ]
-        
-            out_gammas.append(np.max(max_gamma))
-            
-    df = pd.DataFrame(out_res, 
-                      index = pre.times, 
-                      columns = ["all", "nowind", 
-                                 "noreco", "nowindReco", 
-                                 "local"])
-
-    df.to_csv("database/growthRates/gammas250_350km.txt", 
-              sep = ",", 
-              index = True)
-infile = "database/PRE/FZ_PRE_2014_2015.txt"
-
-pre = PRE(infile)
-run_for_all_days(pre)
+df.to_csv("database/growthRates/reco_and_vp_alltimes.txt",
+          sep = ",", index = True)
