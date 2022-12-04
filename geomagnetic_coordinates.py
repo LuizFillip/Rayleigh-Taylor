c36c0fe223de44f9084ab85fc0fb8ec32d7ad033
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-12-04 18:14:26 -0300

    main

--- geomagnetic_coordinates.py
+++ geomagnetic_coordinates.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from iri2016 import IRI
 from geomagnetic_parameters import mag2geo
+import datetime
 
 def density_integrated(Ne, lat, h):
     Re = 6378.165
@@ -34,77 +35,17 @@ def apex_range(h, num = 200):
 
 
 
-
-import datetime
-dz = 50
-altkmrange = (200, 700, dz)
-time = datetime.datetime(2014, 1, 1)
-    
-heights = np.arange(200, 700 + dz, dz)
-
-out_heights = []
-    
-for h in heights:
-    lats, apex = apex_range(h, num = 15)
-    #ax.plot(lats, apex, color = "k")    
-    
-    #ax.contourf(df.columns, df.index, df.values, 40, cmap = "jet")
-    #ax.set(ylim = [200, 700], xlim = [0, 15])
-    
-    lon = -80
-    density_out = []
-    for lat in lats:
-        if lat > 0:
-            geo_lat, geo_lon = mag2geo(lat, lon, time)
-            iri = IRI(time, altkmrange, geo_lat, geo_lon)
-            density_out.append(density_integrated(iri.ne.values, lat, h))
-    
-    sum_lats = np.vstack(density_out).sum(axis = 0)
-    
-    out_heights.append(sum_lats)
-    
-    
-#out.append()
-#fig, ax = plt.subplots()    
-
-res = np.vstack(out_heights) #.T.sum(axis = 0)
+def main():
+    dz = 50
         
-
-print(res)
-
-
-dat = np.vstack(out)
-
-
-N = np.sum(dat, axis = 1)
-
-#print(len(alts), len(N))
-plt.plot(N, alts[:-2])
-
-plt.xscale("log")
-#plt.scale("log")
-
-
-def iri_matrix(infile_iri = "database/iri/"):
-
-    def read_iri(infile):
-        iri = pd.read_csv(infile, 
-                     delim_whitespace=True, 
-                     header = None, index_col= 0)
-        
-        return iri
+    heights = np.arange(200, 700 + dz, dz)
     
-    out_iri = []
-    for mlat in  np.arange(0, 32.5, 2.5): 
-        filename = f"mlat_{float(mlat)}.txt"
+    
+    fig, ax = plt.subplots()   
         
-        dat = read_iri(infile_iri + filename)
-        
-        dat.rename(columns = {1 : -mlat}, 
-                   inplace = True)
-        dat.index.name = "alts"
-        dat.columns.name = "lats"
-        out_iri.append(dat.loc[:, -mlat])
-        
-        
-    return pd.concat(out_iri, axis = 1)
+    for h in heights:
+        lats, apex = apex_range(h, num = 15)
+        ax.plot(lats, apex, color = "k")    
+    
+   
+  
