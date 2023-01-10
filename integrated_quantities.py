50468bdc9c17eefe6ededbf335a8d8201cbc469d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-10 19:20:46 -0300

    testing another method

--- integrated_quantities.py
+++ integrated_quantities.py
@@ -1,63 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.constants as const
-import pandas as pd
 from generic import read_iri
 
-def integrated_values(alts, date, glon = 0, 
+from mag_parameters import apex_latitude, apex_range
+
+
+
+def integrated_density(alts, date, glon = 0, 
                       lat_max = 30, 
                       delta = 5):
     
 
-    out = []
-    for glat in np.arange(0, lat_max + delta, delta):
-        mlat = string_to_list(0, glat, glon, date)[0]
-    
-        sigma_p = get_profiles(float(glat), glon, date, alts)
+    Ne = []
+
+    for i in range(0, 35, 5):
         
-        sigma_1 = sigma_p.values * (1 + 3 * np.sin(np.radians(mlat))**2)
+        df = read_iri(f"database/density_magnetic/{i}.txt")
+        alts = df[0].values 
+        Ne.append(df[1].values* (1 - np.sin(np.radians(i))**2)**3)
         
-        out.append(sigma_1)
-    
-   
+
+    ne = np.vstack(Ne).T
+    delta = 5
     L = 1
-    
-    integrated = 2*Re*L*(np.sum(np.vstack(out).T, axis = 1))*delta
+    Re = 6.31e6
+    integrated = 2*Re*L*np.sum(ne, axis = 1)*delta
+
+    #plt.plot(integrated, alts)
+
+    #plt.xscale("log")
+    mlats = np.arange(0, 35, 5)
    
-    return sigma_p.index, integrated
+    return alts, integrated
    
 
-
-from mag_parameters import apex_latitude, apex_range
-
-Ne = []
-
-for i in range(0, 35, 5):
-    
-    df = read_iri(f"database/density_magnetic/{i}.txt")
-    alts = df[0].values 
-    Ne.append(df[1].values* 1e-6)
-    
-
-ne = np.vstack(Ne).T
-mlats = np.arange(0, 35, 5)
-
-def interpolate_data():
-    from scipy import interpolate
-    
-    X = np.arange(0, 35, 5)
-    Y = alts
-    
-    x,y = np.meshgrid(X,Y)
-    
-    
-    f = interpolate.interp2d(x, y, ne, kind='cubic')
-    
-    Xnew = np.linspace(0, 35, 50)
-    Ynew = np.linspace(100, 700, 1000)
-    
-    
-    return f(Xnew,Ynew)
 
 
 
@@ -82,4 +58,3 @@ def plotContourfMagLines(alts, mlats, ne):
     ax.set(xlim = [0, 20], ylim = [100, 700])
     
     
-plotContourfMagLines(Ynew, Xnew, neI)
