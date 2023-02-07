ac0f5326f2b1adbfb7a8e088892d951f4932548e
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-06 22:59:01 -0300

    new way for compute funcs

--- core.py
+++ core.py
@@ -1,9 +1,7 @@
-import pandas as pd
-import numpy as np
 import datetime as dt
-from common import get_wind, get_pre, runMSISE
+from common import get_wind, get_pre, get_ne, run_msise
 from base.neutral import eff_wind, recombination, nui_1
-
+from base.iono import scale_gradient
 
 def growth_rate_RT(nu, L, R, Vp, U):
     """
@@ -21,44 +19,27 @@ def growth_rate_RT(nu, L, R, Vp, U):
     return (Vp - U + (9.81 / nu))*L - R
 
 
-date_time = dt.datetime(2014, 1, 1, 21, 20)
-
-w = get_wind(date_time.date())
-
-vzp = get_pre(date_time.date())
-
-u = eff_wind(w.zon, w.mer)
-n = runMSISE(date_time)
-R = recombination(n.O2, n.N2)
-
-nu = nui_1(n.Tn, n.O, n.O2, n.N2)
-
-
-print(nu)
+def compute(date_time, func_wind = "Nogueira"):
     
-
-
-class loadNe(object):
+    U = get_wind(date_time, 
+                 func_wind=func_wind).U
     
-    def __init__(self, filename):
-        df = pd.read_csv(filename, index_col = 0)
+    vzp = get_pre(date_time.date())  
+    n = run_msise(date_time, hmin = 200, hmax = 500)
+    R = recombination(n.O2, n.N2)
+
+    nu = nui_1(n.Tn, n.O, n.O2, n.N2)
+
+    ne = get_ne(date_time)
+    
+    l = scale_gradient(ne, dz = 1)
         
-        df["date"] = pd.to_datetime(df["date"])
-        
-        self.times = np.unique(df.date)
-        self.df  = df
-      
-    @property
-    def pivot(self):
-        self.df["alts"] = self.df.index
-        return pd.pivot_table(self.df, 
-                              columns = "date", 
-                              index = "alts", 
-                              values = "Ne")
-    
-    def sel_time(self, date):
-        return self.df.loc[self.df["date"] == date, "Ne"]
-                              
+    return growth_rate_RT(nu.values, l, 
+                          R.values, vzp, 
+                          U.values)
 
 
+date_time = dt.datetime(2014, 1, 1, 21, 10)
+gamma = compute(date_time)
 
+print(gamma)
