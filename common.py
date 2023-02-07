ad5acb6af13228f58c38234ebf7ef4281cbea649
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-06 22:09:40 -0300

    renaming

---
+++ common.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from PlanetaryIndices.base import get_indices
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+from nrlmsise00 import msise_flat
+
+
+def get_pre(date):
+    infile ="database/PRE/FZ_PRE_2014.txt"
+    
+    df = pd.read_csv(infile, index_col = 0)
+    
+    df.index = pd.to_datetime(df.index)
+    
+    ts = df.loc[df.index.date == date, "vz"]
+
+    return ts.item()
+
+
+def get_wind(date, 
+             year = 2014, 
+             site = "for"):    
+    
+    infile = f"database/pyglow/{site}_winds_{year}.txt"
+
+    df = pd.read_csv(infile, index_col = "time")
+    
+    try:
+        del df["Unnamed: 0"]
+    except:
+        pass
+    
+    df.index = pd.to_datetime(df.index)
+    
+    if isinstance(date, dt.datetime):
+        date = date.date()
+        
+    return df.loc[df.index.date == date]
+
+def plot_all(winds, u):
+    winds.zon.plot(label = "zonal")
+    winds.mer.plot(label = "meridional")
+    u.Nogueira.plot(label = "Nogueira")
+    u.Carrasco.plot(label = "Carrasco")
+    u.Jonas.plot(label = "Jonas")
+    plt.legend()
+    
+    
+def runMSISE(datetime, 
+          hmin = 100, 
+          hmax = 600, 
+          step = 1, 
+          glat = -3.73, 
+          glon = -38.522):
+    
+    """Running models MSISE00"""
+    
+    alts = np.arange(hmin, hmax + step, step)
+     
+    t = get_indices(datetime.date())
+    
+    res = msise_flat(datetime, alts[None, :], 
+                     glat, glon, 
+                     t.get("F10.7a"), 
+                     t.get("F10.7obs"), 
+                     t.get("Ap"))
+    
+    columns = ["He", "O", "N2", "O2", "Ar", 
+              "mass", "H", "N", "AnO", "Tex", "Tn"]
+    
+    df = pd.DataFrame(res[0], index = alts, 
+                      columns = columns)
+    
+    df.drop(["He", "Ar", 
+             "mass", "H", "N", "AnO", "Tex"], 
+            axis = 1, 
+            inplace = True)
+    
+    return df
+
