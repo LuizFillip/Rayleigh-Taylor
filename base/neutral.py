63a8fdf893648b8dc854843a0cb245f5778baec8
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-03-18 15:50:51 -0300

    plot folder

--- base/neutral.py
+++ base/neutral.py
@@ -15,27 +15,6 @@ def nui_1(Tn, O, O2, N2):
         
     return term_O + term_O2 + term_N2
 
-def nu_3(O, Tn, Ti):
-    """
-    Equation by Schunk and Nagy, 2000
-
-    Parameters
-    ----------
-    O :  float array
-        Molecular oxygen concentration
-    Tn : float array
-        neutral temperature
-    Ti : float array
-        ion temperature
-    Returns
-    -------
-    TYPE: float array
-        ion-neutral collision frequency.
-
-    """
-    Tr = (Ti + Tn) / 2
-    return 3.7e-11 * O * np.sqrt(Tr) * (1 - 0.064 * np.log10(Tr))**2
-
 def nui_2(O, O2, N2):
     """
     The ion-neutral collision frequency
@@ -51,44 +30,23 @@ def nui_2(O, O2, N2):
     
     return term_N2 + term_O2 + term_O
 
-    
-class eff_wind(object):
-    
+def nu_3(O, Tn, Ti):
     """
-    Effective wind along magnetic fieldÇ
-    
-    Pag. 27 e 28 (Tese da Claudia)
-    U_theta = meridional component (positiva para sul)
-    U_phi = zonal component (positiva para leste)
-    
+    The ion-neutral collision frequency
+    by Schunk and Nagy, 2000
+
+    Parameters
+    ----------
+    O :  float array
+        Molecular oxygen concentration
+    Tn : float array
+        neutral temperature
+    Ti : float array
+        ion temperature
     """
-    
-    def __init__(self, 
-                 zon, 
-                 mer, 
-                 year = 2013,
-                 site = "saa"):
-    
-        d, i = run_igrf(year, site = site)
-    
-        self.D = np.radians(d)
-        self.I = np.radians(i)
-        self.zon = zon #phi, y
-        self.mer = mer #theta, x
-        
-        print(self.D, self.I)
-    
-   
-    @property
-    def eff_zonal(self): 
-        # positiva para leste
-        return (self.zon * np.cos(self.D) + 
-                self.mer * np.sin(self.D))
-    @property
-    def eff_meridional(self):
-        # positiva para sul
-        return (self.mer * np.cos(self.D) -
-                self.zon * np.sin(self.D)) * np.cos(self.I)
+    Tr = (Ti + Tn) / 2
+    return 3.7e-11 * O * np.sqrt(Tr) * (1 - 0.064 * np.log10(Tr))**2
+
 
 def plasma_diffusion(nui):
     """
--- base/winds.py
+++ base/winds.py
@@ -1,17 +1,38 @@
 import numpy as np 
+from GEO.core import run_igrf
+import pandas as pd
+
+d, i = run_igrf(year = 2013, site = "saa")
+
+class effective_wind(object):
+    
+    """
+    Effective wind along magnetic field
+    
+    Pag. 27 e 28 (Tese Ely, 2016)
+    
+    U_theta (mer) = meridional component (positiva para sul)
+    U_phi (zon) = zonal component (positiva para leste)
+    
+    """
+    
+    @staticmethod
+    def eff_zonal(zon, mer, d): 
+        D = np.radians(d)
+        # positiva para leste
+        return (zon * np.cos(D) + mer * np.sin(D))
+    
+    @staticmethod
+    def eff_meridional(zon, mer, d, i):
+        D = np.radians(d)
+        I = np.radians(i)
+        # Ueff_x (positiva para sul)
+        return (
+            mer * np.cos(D) - zon * np.sin(D)
+                ) * np.cos(I)
 
     
-def general_vz(vz, uy, wd, i):
-    """
-    A general expression for vertical plasma drift including
-    the effect from meridional wind and plasma diffusion
-    """
-    I = np.radians(i)
-    
-    vz_term = vz * np.cos(I)
-    uy_term = uy * np.cos(I)* np.sin(I)
-    wd_term = wd * pow(np.sin(I), 2)
-    
-    return vz_term + uy_term - wd_term 
+winds = effective_wind()
+
 
 
