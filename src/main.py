fead82ceecd8c2e4a7ed1880da6a3c04ea508bc4
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-03-24 13:46:38 -0300

    new function in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np 
 from build import paths as p 
-from RayleighTaylor.core import df_parameters
-
-
+import os 
+from AllSky.labeling import save_img
+import datetime as dt
 
 def get_max(df, date, alts = (250, 350)):
    
@@ -29,3 +29,30 @@ def run(df):
             np.array(out, dtype = np.float64))
     
 
+def save_timeseries_parameters(
+        iri_infile = "D:\\ne\\2013\\",
+        save_in = "D:\\plots\\"):
+    
+    
+    files = os.listdir(iri_infile)
+    
+    for filename in files:
+    
+        date = dt.datetime.strptime(
+            filename.replace(".txt", ""), "%Y%m%d"
+            )
+        
+        dn = date + dt.timedelta(hours = 20)
+        
+        try:
+            iri_file = os.path.join(iri_infile, filename)
+            fig = plot_timeseries_parameters(
+                    dn, iri_file)
+            print("saving...", dn)
+            fig_name = filename.replace(".txt", ".png")
+            save_img(
+                fig, 
+                os.path.join(save_in, fig_name)
+                     )
+        except:
+            continue
