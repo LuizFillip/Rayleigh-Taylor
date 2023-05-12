04ce661cf7d0de3853538d3c0e681613b997411c
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-11 22:30:55 -0300

    equations on fluxtube

---
+++ src/equations.py
@@ -0,0 +1,8 @@
+
+
+fluxtube_labels = ["$(V_{zp} - U + \frac{g}{\nu_{in}})\frac{1}{n_e} \frac{\partial n_e}{\partial y} - R$",
+         "$(V_{zp} + \frac{g}{\nu_{in}})\frac{1}{n_e} \frac{\partial n_e}{\partial y}$", 
+         "$(- U + \frac{g}{\nu_{in}})\frac{1}{n_e} \frac{\partial n_e}{\partial y}$ - R", 
+            "$(V_{zp} - U + \frac{g}{\nu_{in}})\frac{1}{n_e} \frac{\partial n_e}{\partial y}$", 
+            "$(V_{zp} + \frac{g}{\nu_{in}})\frac{1}{n_e} \frac{\partial n_e}{\partial y} - R$", ]
+  
