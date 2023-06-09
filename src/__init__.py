87e53a323dded49c0ec2fff40154176a6c47b946
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-06-09 03:16:58 -0300

    update init

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,14 @@
+from .base import set_data, split_by_freq, load_process
+from .LabelsRT import EquationsFT, EquationsRT
+from .gamma_forms import gamma_forms
+
 from .RT import (
     all_effects,
     effects_due_to_winds, 
     effects_due_to_drift,
     effects_due_to_gravity
     )
-from .base import set_data, split_by_freq, load_process
-from .LabelsRT import EquationsFT, EquationsRT
+
 
 import settings as s 
 
