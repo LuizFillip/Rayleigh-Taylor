831dd4c471e0512c907f069d8e680136b0005fc5
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-14 14:38:00 -0300

    Construct Fluxtube growth rate

--- src/parametrics.py
+++ src/parametrics.py
@@ -1,13 +1,59 @@
-import RayleighTaylor as rt
-from base import sel_times, load
+import base as b
 import datetime as dt
+import os
+import FluxTube as ft
+
+PATH_PRE = 'digisonde/data/PRE/'
+PATH_FLUXTUBE =  'FluxTube/data/reduced/'
+
+def PRE(year, site = 'saa', alt = 300):
+    
+    infile = os.path.join(
+        PATH_PRE,
+        site,
+        f'R{year}.txt'
+        )
+
+    ds = b.load(infile)
+    
+    ds.rename(
+        columns  = {'vzp': 'vp'}, 
+        inplace = True
+        )
+
+    ds['vp'] = ds['vp'] * ft.factor_height(alt)**3
+
+    return ds
+
+def FluxTube_dataset(
+        dn, 
+        site = 'saa'
+        ):
+    
+    infile = os.path.join(
+        PATH_FLUXTUBE,
+        site, 
+        f'{dn.year}.txt'
+        )
+    ds = b.load(infile)
+    
+    try:
+        joined = ds.join(PRE(dn.year))
+    except:
+        joined = ds.copy()
+    
+    return b.sel_times(joined, dn)
 
 
-infile = 'FluxTube/data/reduced/saa/2019.txt'
-dn = dt.datetime(2019, 1, 1, 20)
+import datetime as dt 
 
-df = rt.gammas_integrated(sel_times(load(infile), dn))
+dn = dt.datetime(2013, 1, 1, 21)
+ds = FluxTube_dataset(dn, site = 'jic')
+    
+    
+# df = rt.gammas_integrated(ds)
 
-df['gravity'] = df['gravity']*1e4
 
-df['gravity'].plot()
+# ds['K'].plot()
+
+ds.columns
