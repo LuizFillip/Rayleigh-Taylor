9b51d75ee123e0b7382f042cd9cd81d715579631
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-02-25 16:24:15 -0300

    load new funcs

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from .eq_labels import EquationsFT, EquationsRT
 from .RT import *
 from .parametrics import *
 # from .loading import *
-from .local_method import local_gamma, pre_effects, sel_times
+from .local_method import local_results
